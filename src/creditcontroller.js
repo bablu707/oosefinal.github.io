380cb022068114b2f83a4fcdfadfe7d7952d02ae
Ayrus707 <122613157+Ayrus707@users.noreply.github.com>
2024-04-28 16:55:22 +0530

    firstpush
    code1_push

---
+++ src/creditcontroller.js
@@ -0,0 +1,51 @@
+// const mongoose=require("mongoose");
+// const db="mongodb+srv://oosedatabase:Vlnsrra8SIpPeBjt@cluster0.ljekmsz.mongodb.net/";
+// const connect=mongoose.connect(db);
+// connect.then(()=>{
+//     console.log("Database Successfully Connected-controller");
+// })
+// .catch(()=>{
+//     console.log("Some error Occured!-controller");
+// });
+/*const creditSchema = new mongoose.Schema({
+    DeviceID: {
+        type: Number,
+        required: true
+    },
+    metaltype: {
+        type: String,
+        required: true
+    },
+    credit: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: String,
+        required: true
+    }
+});*/
+
+
+// const deviceSchema = new mongoose.Schema({
+//     _id: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         required: true
+//     },
+//     DeviceID: {
+//         type: Number,
+//         required: true
+//     },
+//     DeviceType: {
+//         type: String,
+//         required: true
+//     },
+//     brand: {
+//         type: [String],
+//         required: true
+//     }
+// });
+
+// const credit = mongoose.model("device", deviceSchema);
+
+// module.exports = credit;
